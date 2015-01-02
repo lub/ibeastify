d9e1fe54228f5ef7f53390bba83d4afb64fcce56
lub <github@lubiland.de>
2015-01-02 04:27:58 +0100

    init

---
+++ pushwoosh.js
@@ -0,0 +1,17 @@
+function initPushwoosh() {
+	var pushNotification = window.plugins.pushNotification;
+	
+	//initialize Pushwoosh with projectid: GOOGLE_PROJECT_ID, appid : PUSHWOOSH_APP_ID. This will trigger all pending push notifications on start.
+	pushNotification.onDeviceReady({ projectid: 'apt-rope-813', appid : 'B6619-BACBF' });
+	
+	//register for pushes
+	pushNotification.registerDevice(
+		function(status) {
+			var pushToken = status;
+		},
+		function(status) {
+		}
+	);
+}
+
+document.addEventListener('initPushwoosh', init, true);
